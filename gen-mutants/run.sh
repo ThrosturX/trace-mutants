53a35c791f55b178d0f79e970fd9c9f8932bb736
throsturx <sparog+github@gmail.com>
2015-04-16 19:40:08 +0000

    index fix for rebase

--- gen-mutants/run.sh
+++ gen-mutants/run.sh
@@ -15,8 +15,10 @@ java mujava.cli.genmutes -ALL $NAME
 
 mkdir mutants
 
+
 for src in "${SRCS[@]}"; do
 	cp $NAME/result/$src/traditional_mutants/*/*/$src.java mutants --parents
+	cp $NAME/result/$src/traditional_mutants/mutation_log mutants
 done
 
 tar -cf mutants.tar mutants 
